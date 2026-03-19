ea6719ff54c9a8fed1db9a1c84f9479fb845a119
awais-koder <awaistayyab27@gmail.com>
2026-03-19 15:27:03 +0500

    first commit

---
+++ config/system-addons.php
@@ -0,0 +1,66 @@
+<?php return array (
+  'admin_panel' => 
+  array (
+    'active' => '1',
+    'name' => 'MUHAMMAD AWAIS',
+    'identifier' => 'awaistayyab27@gmail.com',
+    'username' => '3110115867551',
+    'purchase_key' => 'b86667cc-8ccd-433a-82b4-a64d84514771',
+    'software_id' => 'MzE0NDg1OTc=',
+    'domain' => '127.0.0.1:8000',
+    'software_type' => 'product',
+    'errors' => 
+    array (
+    ),
+  ),
+  'vendor_panel' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'user_app' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'vendor_app' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'deliveryman_app' => 
+  array (
+    'active' => '1',
+    'name' => '',
+    'identifier' => '',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => 'MzYwODUwNDE=',
+    'domain' => '',
+    'software_type' => 'addon',
+    'errors' => 
+    array (
+    ),
+  ),
+  'theme_lifestyle' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+);
